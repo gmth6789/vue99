e9ffd4f2b88be39d37994f003c78341d5dc9194d
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-08-28 00:33:06 +0700

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,55 @@
 
 
 
+
+
+<div class="flex diygw-col-24 diygw-bottom">
+					<div class="diygw-grid diygw-actions">
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="/static/grid1.png" />
+								</div>
+								<div class="diygw-grid-title">หน้าหลัก</div>
+							</div>
+						</button>
+            <button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/grid2.png" />
+								</div>
+								<div class="diygw-grid-title">菜单二</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/grid3.png" />
+								</div>
+								<div class="diygw-grid-title">菜单三</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/grid4.png" />
+								</div>
+								<div class="diygw-grid-title">菜单四</div>
+							</div>
+						</button>
+
+
+
+            </div>
+            </div>
+				
+
+
+				<div class="version text-grey flex-direction-column flex diygw-col-24 justify-center align-center">
+					<div>แพลตฟอร์มสร้างรายได้ออนไลน์</div>
+					<div>https://www.example.com</div>
+				</div>
+				<div class="clearfix"></div>
    </div>
   </div>
 </template>
