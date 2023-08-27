27c48d253585462cac124442eb1299a985f4d83d
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-08-28 00:35:29 +0700

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
 						<button class="diygw-action">
 							<div class="diygw-grid-inner">
 								<div class="diygw-grid-icon diygw-avatar" style="">
-									<img mode="aspectFit" class="diygw-avatar-img" src="/static/grid1.png" />
+									<img mode="aspectFit" class="diygw-avatar-img" src="/static/grid1.png">
 								</div>
 								<div class="diygw-grid-title">หน้าหลัก</div>
 							</div>
